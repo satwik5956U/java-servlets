59c05870ff0c8188e8b6e58297c4d2bab65904e3
satwik5956U <vvit20bq1a05g8@gmail.com>
2022-07-13 21:03:51 +0530

    added Demo.java

---
+++ newfolder/Demo.java
@@ -0,0 +1,11 @@
+package satwik2;
+import java.util.Date;
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date d=new Date();
+		System.out.println(""+d);
+	}
+
+}
